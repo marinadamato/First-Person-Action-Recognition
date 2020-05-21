e6e515ad6cdf0ef6c58e4eab735c4a907c71036b
Pikkius <andrea.arcidiacono.it@gmail.com>
2020-05-21 20:32:11 +0200

    Update makeDatasetTwoStream.py

--- makeDatasetTwoStream.py
+++ makeDatasetTwoStream.py
@@ -118,7 +118,7 @@ class makeDataset(Dataset):
             inpSeqSegs = torch.stack(inpSeq, 0).squeeze(1)
         inpSeqF = []
         for i in np.linspace(1, numFrame, self.seqLen, endpoint=False):
-            fl_name = vid_nameF + '/' + 'image_' + str(int(np.floor(i))).zfill(5) + self.fmt
+            fl_name = vid_nameF + '/' + 'rgb' + str(int(np.floor(i))).zfill(4) + self.fmt
             img = Image.open(fl_name)
             inpSeqF.append(self.spatial_transform(img.convert('RGB')))
         inpSeqF = torch.stack(inpSeqF, 0)
