c5f2b1e94e7e184bcd9d5fe1eef774b63f8c3681
Marina D'Amato <43416377+marinadamato@users.noreply.github.com>
2020-05-25 16:46:20 +0200

    Update makeDatasetMS.py

--- makeDatasetMS.py
+++ makeDatasetMS.py
@@ -54,9 +54,9 @@ class makeDataset(Dataset):
         self.images, self.labels, self.maps, self.numFrames = gen_split(root_dir, 5,phase)
         normalize = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         self.spatial_transform0 = spatial_transform
-        self.spatial_rgb= Compose([spatial_transform0, ToTensor(), normalize])
+        self.spatial_rgb= Compose([self.spatial_transform0, ToTensor(), normalize])
                                
-        self.spatial_transform_map = Compose([spatial_transform0, Scale(7), ToTensor()])
+        self.spatial_transform_map = Compose([self.spatial_transform0, Scale(7), ToTensor()])
         
         
         
