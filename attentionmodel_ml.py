5aa5c09723c28103fb8de4bf06989970c289303a
Pikkius <andrea.arcidiacono.it@gmail.com>
2020-05-26 17:07:42 +0200

    Update attentionmodel_ml.py

--- attentionmodel_ml.py
+++ attentionmodel_ml.py
@@ -5,7 +5,7 @@ from torch.nn import functional as F
 from torch.autograd import Variable
 from MyConvLSTMCell import *
 
-class msNet(nn.Module):
+'''class msNet(nn.Module):
     def __init__(self):
         super(msNet, self).__init__()
         self.conv = nn.Sequential(nn.ReLU(inplace=True),
@@ -18,7 +18,7 @@ class msNet(nn.Module):
         x = self.conv(x)
         x = x.view(x.size(0), -1)
         x = self.clas(x)
-        return x
+        return'''
 
 
 
@@ -26,7 +26,6 @@ class attentionModel_ml(nn.Module):
     def __init__(self, num_classes=61, mem_size=512):
         super(attentionModel_ml, self).__init__()
         self.num_classes = num_classes
-        self.msNet= msNet()
         self.resNet = resnetMod.resnet34(True, True)
         self.mem_size = mem_size
         self.weight_softmax = self.resNet.fc.weight
@@ -35,6 +34,13 @@ class attentionModel_ml(nn.Module):
         self.dropout = nn.Dropout(0.7)
         self.fc = nn.Linear(mem_size, self.num_classes)
         self.classifier = nn.Sequential(self.dropout, self.fc)
+
+        #msNet
+        self.conv = nn.Sequential(nn.ReLU(inplace=True),
+            nn.Conv2d(512, 100, 3, stride=1, padding=1, dilation=1, groups=1, bias=True))
+        self.clas=nn.Sequential(
+            nn.Linear(7*7*100,49),
+            nn.Softmax(1))
 
     def forward(self, inputVariable):
         state = (Variable(torch.zeros((inputVariable.size(1), self.mem_size, 7, 7)).cuda()),
@@ -49,7 +55,11 @@ class attentionModel_ml(nn.Module):
             attentionMAP = F.softmax(cam.squeeze(1), dim=1)
             attentionMAP = attentionMAP.view(attentionMAP.size(0), 1, 7, 7)
             attentionFeat = feature_convNBN * attentionMAP.expand_as(feature_conv)
-            output_msnet=self.msNet.forward(attentionFeat)
+            
+            output_msnet = self.conv(attentionFeat)
+            output_msnet = output_msnet.view(output_msnet.size(0), -1)
+            output_msnet = self.clas(output_msnet)
+
             state = self.lstm_cell(attentionFeat, state)
         
         feats1 = self.avgpool(state[1]).view(state[1].size(0), -1)
