16179addec66197b5f5a73c43018f36aa68d2516
Marina D'Amato <43416377+marinadamato@users.noreply.github.com>
2020-05-28 17:09:03 +0200

    Update mainMsTask.py

--- mainMsTask.py
+++ mainMsTask.py
@@ -170,7 +170,7 @@ def main_run(dataset, stage, train_data_dir, val_data_dir, stage1_dict, out_dir,
                     loss_ms.backward()
                     epoch_loss_ms+=loss_ms.data[0]
                 else:
-                    loss_ms=loss_fn(output_ms, binary_map)
+                    loss_ms=loss_fn(output_ms.view(seqLen, trainBatchSize, 1, 7, 7), binary_map)
                     loss_ms.backward()
                     epoch_loss_ms+=loss_ms.data[0]
         
