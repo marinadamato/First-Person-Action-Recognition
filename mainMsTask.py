e66e160ede72efe50a95fc95c60fd4247bd41872
Marina D'Amato <43416377+marinadamato@users.noreply.github.com>
2020-05-28 17:12:15 +0200

    Update mainMsTask.py

--- mainMsTask.py
+++ mainMsTask.py
@@ -207,11 +207,11 @@ def main_run(dataset, stage, train_data_dir, val_data_dir, stage1_dict, out_dir,
                     binary_map = Variable(binary_map.permute(1, 0, 2, 3, 4).cuda())
                     if stage==2:
                         if regressor:
-                            loss_ms=loss_reg(output_ms.view(seqLen, trainBatchSize, 1, 7, 7), binary_map)
+                            loss_ms=loss_reg(output_ms.view(seqLen, valBatchSize, 1, 7, 7), binary_map)
                             loss_ms.backward()
                             epoch_loss_ms+=loss_ms.data[0]
                         else:
-                            loss_ms=loss_fn(output_ms.view(seqLen, trainBatchSize, 1, 7, 7), binary_map)
+                            loss_ms=loss_fn(output_ms.view(seqLen, valBatchSize, 1, 7, 7), binary_map)
                             loss_ms.backward()
                             epoch_loss_ms+=loss_ms.data[0]
                                 
