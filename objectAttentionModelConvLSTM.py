a6638a739a1afe32beb3f84bf85a545580939eaf
Marina D'Amato <43416377+marinadamato@users.noreply.github.com>
2020-06-01 17:52:52 +0200

    Update objectAttentionModelConvLSTM.py

--- objectAttentionModelConvLSTM.py
+++ objectAttentionModelConvLSTM.py
@@ -7,9 +7,10 @@ from MyConvLSTMCell import *
 
 
 class attentionModel(nn.Module):
-    def __init__(self, num_classes=61, mem_size=512):
+    def __init__(self, num_classes=61, mem_size=512, attention=True):
         super(attentionModel, self).__init__()
         self.num_classes = num_classes
+        self.attention = attention
         self.resNet = resnetMod.resnet34(True, True)
         self.mem_size = mem_size
         self.weight_softmax = self.resNet.fc.weight
@@ -24,15 +25,18 @@ class attentionModel(nn.Module):
                  Variable(torch.zeros((inputVariable.size(1), self.mem_size, 7, 7)).cuda()))
         for t in range(inputVariable.size(0)):
             logit, feature_conv, feature_convNBN = self.resNet(inputVariable[t])
-            bz, nc, h, w = feature_conv.size()
-            feature_conv1 = feature_conv.view(bz, nc, h*w)
-            probs, idxs = logit.sort(1, True)
-            class_idx = idxs[:, 0]
-            cam = torch.bmm(self.weight_softmax[class_idx].unsqueeze(1), feature_conv1)
-            attentionMAP = F.softmax(cam.squeeze(1), dim=1)
-            attentionMAP = attentionMAP.view(attentionMAP.size(0), 1, 7, 7)
-            attentionFeat = feature_convNBN * attentionMAP.expand_as(feature_conv)
-            state = self.lstm_cell(attentionFeat, state)
+            if attention: 
+                bz, nc, h, w = feature_conv.size()
+                feature_conv1 = feature_conv.view(bz, nc, h*w)
+                probs, idxs = logit.sort(1, True)
+                class_idx = idxs[:, 0]
+                cam = torch.bmm(self.weight_softmax[class_idx].unsqueeze(1), feature_conv1)
+                attentionMAP = F.softmax(cam.squeeze(1), dim=1)
+                attentionMAP = attentionMAP.view(attentionMAP.size(0), 1, 7, 7)
+                attentionFeat = feature_convNBN * attentionMAP.expand_as(feature_conv)
+                state = self.lstm_cell(attentionFeat, state)
+            else:
+                state = self.lstm_cell(logit, state)
         feats1 = self.avgpool(state[1]).view(state[1].size(0), -1)
         feats = self.classifier(feats1)
         return feats, feats1
