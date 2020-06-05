7a544737270ecd39a24d61aacebaced3f35d5c69
Pikkius <andrea.arcidiacono.it@gmail.com>
2020-06-05 16:36:14 +0200

    Update makeDatasetColorization.py

--- makeDatasetColorization.py
+++ makeDatasetColorization.py
@@ -3,82 +3,126 @@ import torch
 from torch.utils.data import Dataset
 from PIL import Image
 import numpy as np
-import glob
 import random
-from spatial_transforms import (Compose, ToTensor, CenterCrop, Scale, Normalize, MultiScaleCornerCrop,
-                                RandomHorizontalFlip, Binary)
-from colorization_block import colorization
+import glob
+import sys
+
 
 def gen_split(root_dir, stackSize, phase):
-    RGB = []
+    DatasetX = []
+    DatasetY = []
+    DatasetF = []
     Labels = []
     Maps = []
     NumFrames = []
-    root_dir = os.path.join(root_dir, 'processed_frames2') #GTEA61/processed_frames2/
     
-    for dir_user in sorted(os.listdir(root_dir)): #S1/
-        if dir_user=='.DS_Store': continue
-        if (phase=='train') ^ (dir_user=="S2"):
-            dir = os.path.join(root_dir, dir_user) #GTEA61/processed_frames2/S1/
-            class_id=0
-            
-            for target in sorted(os.listdir(dir)): #close_choco/
-                if target=='.DS_Store': continue
-                dir1 = os.path.join(dir, target) #GTEA61/processed_frames2/S1/close_choco/
-                
-                insts = sorted(os.listdir(dir1)) #1/
-                if insts != []:
-                    for inst in insts:
-                        if inst=='.DS_Store': continue
-                        
-                        inst_dir = os.path.join(dir1, inst+"/rgb") #GTEA61/processed_frames2/S1/close_choco/1/rgb/
-                        numFrames = len(glob.glob1(inst_dir, '*.png'))
-                        
-                        if numFrames >= stackSize:
-                            RGB.append(inst_dir)
-                            Labels.append(class_id)
-                            NumFrames.append(numFrames)
-                            
-                        inst_dir = os.path.join(dir1, inst+"/mmaps") #GTEA61/processed_frames2/S1/close_choco/1/mmaps/
-                        
-                        if numFrames >= stackSize:
-                            Maps.append(inst_dir)
-                class_id += 1
-    return RGB, Maps, Labels, NumFrames 
+    for original_dir in sorted(os.listdir(root_dir)):
+        if (original_dir=='.DS_Store'): continue
+        root_dir1 = os.path.join(root_dir, original_dir) #GTEA61/flow_x_processed/
+        for dir_user in sorted(os.listdir(root_dir1)):
+            if dir_user=='.DS_Store': continue
+            if (phase=='train') ^ (dir_user=="S2"):
+                class_id = 0
+                dir = os.path.join(root_dir1, dir_user) #GTEA61/processed_frames2/S1/
+                for target in sorted(os.listdir(dir)):
+                    if target=='.DS_Store': continue
+                    dir1 = os.path.join(dir, target) #GTEA61/processed_frames2/S1/close_choco/
+                    insts = sorted(os.listdir(dir1))
+                    if insts != []:
+                        for inst in insts:
+                            if inst=='.DS_Store': continue
+                            inst_dir = os.path.join(dir1, inst) #GTEA61/processed_frames2/S1/close_choco/1/
+                            numFrames = len(glob.glob1(inst_dir, '*.png'))
+                            if (original_dir == 'processed_frames2'):
+                                numFrames = len(glob.glob1(inst_dir+'/rgb', '*.png'))
+                            if numFrames >= stackSize:
+
+                                if (original_dir == 'flow_x_processed'):
+                                    DatasetX.append(inst_dir)
+                                if (original_dir == 'flow_y_processed'):
+                                    DatasetY.append(inst_dir)
+                                if (original_dir == 'processed_frames2'):   
+                                    DatasetF.append(inst_dir+'/rgb')
+                                    Labels.append(class_id)
+                                    Maps.append(inst_dir+'/mmaps')
+                                NumFrames.append(numFrames)
+                                   
+                                
+                    class_id += 1
+    return DatasetX, DatasetY, DatasetF, Labels, NumFrames
 
 class makeDataset(Dataset):
-    def __init__(self, root_dir, spatial_transform=None, seqLen=20,
-                 train=True, mulSeg=False, numSeg=1, fmt='.png',phase='train', regressor=False):
+    def __init__(self, root_dir, spatial_transform=None, sequence=False, stackSize=5,
+                 train=True, numSeg=5, fmt='.png', phase='train', seqLen = 25):
+        """
+        Args:
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
 
-        self.images, self.maps, self.labels, self.numFrames = gen_split(root_dir, 5,phase)
-        normalize = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        self.spatial_transform0 = spatial_transform
-        self.spatial_rgb= Compose([self.spatial_transform0, ToTensor(), normalize])
-        
-       
-        self.spatial_transform_map = Compose([self.spatial_transform0, Scale(7), ToTensor()])
-               
-        
+        self.imagesX, self.imagesY, self.imagesF, self.labels, self.numFrames = gen_split(
+            root_dir, stackSize, phase)
+        self.spatial_transform = spatial_transform
         self.train = train
-        self.mulSeg = mulSeg
         self.numSeg = numSeg
-        self.seqLen = seqLen
+        self.sequence = sequence
+        self.stackSize = stackSize
         self.fmt = fmt
+        self.phase = phase
+        self.seqLen = seqLen
 
     def __len__(self):
-        return len(self.images)
+        return len(self.imagesX)
 
     def __getitem__(self, idx):
-        vid_name = self.images[idx]
+        vid_nameX = self.imagesX[idx]
+        vid_nameY = self.imagesY[idx]
+        vid_nameF = self.imagesF[idx]
         label = self.labels[idx]
-        map_name = self.maps[idx]
-
         numFrame = self.numFrames[idx]
-        inpSeq = []
-        mapSeq = []
-        self.spatial_transform0.randomize_parameters()
+        inpSeqSegs = []
+        self.spatial_transform.randomize_parameters()
+        if self.sequence is True:
+            if numFrame <= self.stackSize:
+                frameStart = np.ones(self.numSeg)
+            else:
+                frameStart = np.linspace(1, numFrame - self.stackSize, self.numSeg)
+            for startFrame in frameStart:
+                inpSeq = []
+                for k in range(self.stackSize):
+                    i = k + int(startFrame)
+                    fl_name = vid_nameX + '/flow_x_' + str(int(round(i))).zfill(5) + '.png'
+                    img = Image.open(fl_name)
+                    inpSeq.append(self.spatial_transform(img.convert('L'), inv=True, flow=True))
+                    # fl_names.append(fl_name)
+                    fl_name = vid_nameY + '/flow_y_' + str(int(round(i))).zfill(5) + '.png'
+                    img = Image.open(fl_name)
+                    inpSeq.append(self.spatial_transform(img.convert('L'), inv=False, flow=True))
+                inpSeqSegs.append(torch.stack(inpSeq, 0).squeeze())
+            inpSeqSegs = torch.stack(inpSeqSegs, 0)
+        else:
+            if numFrame <= self.stackSize:
+                startFrame = 1
+            else:
+                if self.phase == 'train':
+                    startFrame = random.randint(1, numFrame - self.stackSize)
+                else:
+                    startFrame = np.ceil((numFrame - self.stackSize)/2)
+            inpSeq = []
+            for k in range(self.stackSize):
+                i = k + int(startFrame)
+                fl_name = vid_nameX + '/flow_x_' + str(int(round(i))).zfill(5) + '.png'
+                img = Image.open(fl_name)
+                # fl_names.append(fl_name)
+                f2_name = vid_nameY + '/flow_y_' + str(int(round(i))).zfill(5) + '.png'
+                img2 = Image.open(f2_name)
+                inpSeq.append((self.spatial_transform(img.convert('L'), inv=False, flow=True),
+                               self.spatial_transform(img2.convert('L'), inv=False, flow=True)))
+            inpSeqSegs = torch.stack(inpSeq, 0).squeeze(1)
+        inpSeqF = []
         for i in np.linspace(1, numFrame, self.seqLen, endpoint=False):
-            fl_name = vid_name + '/' + 'rgb' + str(int(np.floor(i))).zfill(4) + self.fmt
+            fl_name = vid_nameF + '/' + 'rgb' + str(int(np.floor(i))).zfill(4) + self.fmt
             img = Image.open(fl_name)
             flag=1
             j=i
@@ -95,11 +139,8 @@ class makeDataset(Dataset):
                         j= 2*i-j #j=i+1 j-i=1 --> j=i-1
                     continue
 
-            inpSeq.append(self.spatial_rgb(img.convert('RGB')))
-            img_bew= self.spatial_transform_map(img.convert('L'))
-            colorized_image=colorization(img_bew)
-            
-            mapSeq.append(colorized_image) #Grayscale
-        inpSeq = torch.stack(inpSeq, 0)
+            inpSeqF.append(self.spatial_transform(img.convert('RGB')))
+            mapSeq.append(self.spatial_transform_map(mappa.convert('L'))) #Grayscale
+        inpSeqF = torch.stack(inpSeqF, 0)
         mapSeq = torch.stack(mapSeq, 0)
-        return inpSeq, mapSeq, label
+        return inpSeqSegs, inpSeqF, mapSeq, label#, vid_nameF#, fl_name
